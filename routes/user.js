f879d783c4ef99a106ebeee0920a1cdfe7847c44
Abdullah <abdullahyahaya46@gmail.com>
2022-03-12 12:50:34 +0100

    Done With FrontEnd : Linking the pages, PageStructures, Folder Arrangement, Etc

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/dashboard', (req, res) => {
-    res.render('user/dashboard');
+    res.render('user/account-dashboard');
+})
+
+router.get('/chat', (req, res) => {
+    res.render('user/account-chat');
+})
+
+router.get('/friends', (req, res) => {
+    res.render('user/account-members');
+})
+
+router.get('/track', (req, res) => {
+    res.render('user/account-track');
+})
+
+router.get('/profile', (req, res) => {
+    res.render('user/account-profile');
 })
 
 module.exports = router;
