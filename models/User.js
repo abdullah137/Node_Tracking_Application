169445468c3be54ca05b4eef05d562b38b173c9c
Abdullah <abdullahyahaya46@gmail.com>
2022-04-14 09:16:45 +0100

    Adding the ProfileImge Property

--- models/User.js
+++ models/User.js
@@ -29,6 +29,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profileImg: {
+        type: String,
+        required: false
+    },
     profileIntro: {
         type: String,
         default: ''
