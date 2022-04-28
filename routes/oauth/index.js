18ca4a833417f19ae85c924043f14440a2d1af37
Abdullah <abdullahyahaya46@gmail.com>
2022-04-28 19:39:50 +0100

    Adding Status Routes For Facebook And Github OAuth

--- routes/oauth/index.js
+++ routes/oauth/index.js
@@ -14,12 +14,29 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 
 router.get('/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
 
-router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect: '/users/dashboard', failureRedirect: '/' }));
+router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect: '/users/dashboard', failureRedirect: '/' }), (req, res) => {
+ 
+  const _status = req.user.isStatus
+    
+  if(!_status) {
+      res.redirect('/users/activate');
+  }else {
+      res.redirect('/users/dashboard')
+  } 
+
+});
 
 router.get('/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
 
 router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/user/signup' }), function(req, res) {
-  res.redirect('/users/dashboard');
+
+  const _status = req.user.isStatus
+    
+    if(!_status) {
+        res.redirect('/users/activate');
+    }else {
+        res.redirect('/users/dashboard')
+    }
 })
 
 module.exports = router;
