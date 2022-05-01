09c4081b5b2e8b04bfb9af9bf523f3c1b5ad5bc1
Abdullah <abdullahyahaya46@gmail.com>
2022-05-01 07:55:19 +0100

    Made Configurations for Github Oauth

--- config/oauth_github.js
+++ config/oauth_github.js
@@ -1,5 +1,5 @@
 var GitHubStrategy = require('passport-github2').Strategy;
-var brycpt = require('bcrypt');
+var bcrypt = require('bcrypt');
 
 // Loading User Models
 const Users = require('../models/User');
@@ -9,13 +9,46 @@ module.exports = function(passport) {
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "/users/auth/github/callback"
+        callbackURL: "/users/auth/github/callback",
+        scope: "user:email"
       },
 
-      function(accessToken, refreshToken, profile, cb) {
+     async function(accessToken, refreshToken, profile, done) {
 
-            console.log(profile);
+           const randomOtp = Math.floor(Math.random()*90000)+10000;
 
+           // hash password
+           const salt = await bcrypt.genSalt(10);
+           const hashPassword = await bcrypt.hash(profile.emails[0].value, salt);
+
+           const User = { 
+               userName: profile.id,
+               firstName: profile.displayName,
+               lastName: profile.displayName,
+               email: profile.emails[0].value,
+               password: hashPassword,
+               dob: "0000-00-00",
+               profileImg: ''
+           }
+
+           // Check into the Databse if record exist
+           try {
+
+            let user = await Users.findOne({
+                email: profile.emails[0].value
+            })
+
+            if(user) {
+                done(null, user);
+            }else {
+                user = await Users.create(User)
+                done(null, user)
+            }
+      
+        } catch(error) {
+            console.log(error);
+            console.log("Sorry, An error Occured Here")
+        }
       }
 
     ));
