05216238b6b9ff295712ab7080b30efb48b539e8
Abdullah <abdullahyahaya46@gmail.com>
2022-05-01 08:00:36 +0100

    Adding Canceling Friend Request for users

--- routes/friends/index.js
+++ routes/friends/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Importing our controllers for these
-const { friends, accept, requests, decline } = require('../../controllers/friends/index')
+const { friends, accept, requests, remove, search, cancel } = require('../../controllers/friends/index')
 
 // Ensuring the routes are protected
 const { ensureAuthenticated, forwardAuthenticated }  = require("../../middleware/auth");
@@ -11,8 +11,12 @@ router.get('/', friends);
 
 router.get('/request/:id', requests);
 
-router.get('/decline/:id', decline)
+router.get('/decline/:id', remove)
 
 router.get('/accept/:id', accept);
+
+router.post('/', search);
+
+router.get('/cancel/:id', cancel);
 
 module.exports = router;
