5dd8923f887d37421572bbe05e821c1319fd2b88
Abdullah <abdullahyahaya46@gmail.com>
2022-05-06 04:11:56 +0100

    Redirecting the User's When Registraion Information is not complete

--- controllers/oauth/index.js
+++ controllers/oauth/index.js
@@ -3,7 +3,15 @@ const passport = require('passport');
 const googleOauth = passport.authenticate('google', { scope: ['profile', 'email'] })
 
 const googleCallbackFunction = (req, res) => {
-    res.redirect('/users/dashboard');
+  
+    const _status = req.user.isStatus
+    
+    if(!_status) {
+        res.redirect('/users/activate');
+    }else {
+        res.redirect('/users/dashboard')
+    }
+   
   }
 
 module.exports = { googleOauth,googleCallbackFunction }
